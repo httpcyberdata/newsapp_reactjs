148bfc1c6ab040b330b9b0dd645154a85a6b9974
httpcyberdata <borgeringstad@Brge-Ringstad-PC.local>
2022-11-16 18:07:57 -0500

    created Home.jsx & styles.css and relevant folders

---
+++ src/components/home/mainContent/homes/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <main>
+            <div className="container">
+                <section className="mainContent">
+
+                </section>
+                <section className="sideContent">
+                    
+                </section>
+            </div>
+        </main>
+    </div>
+  )
+}
+
+export default Home;
