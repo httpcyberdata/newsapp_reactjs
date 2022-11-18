fe3b8de8f055f16f129dbb6c5922de71b0096a77
httpcyberdata <borgeringstad@Brge-Ringstad-PC.local>
2022-11-18 17:35:05 -0500

    categories section implemented

--- src/components/home/sideContent/side/Side.jsx
+++ src/components/home/sideContent/side/Side.jsx
@@ -4,6 +4,7 @@ import SocialMedia from '../social/SocialMedia';
 import Tpost from '../tpost/Tpost';
 import './side.css';
 const Side = () => {
+     const category = ["world", "travel", "sport", "fun", "health", "fashion", "business", "technology"];
   return (
     <div>
         <Heading title="Stay Connected" />
@@ -25,6 +26,16 @@ const Side = () => {
             <img src="../images/sidebar-banner-new.jpg"/>
         </section>
         <Tpost />
+        <section className="catgeorys">
+            <Heading title="Categories"/>
+            {category.map((val) => {
+                return (
+                    <div className="category category1">
+                    <span>{val}</span>
+                    </div>
+                )
+            })}
+        </section>
     </div>
   )
 }
