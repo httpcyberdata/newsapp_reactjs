367c1f5e60e81201ad31c7beafcb61bf1aa8d8a1
httpcyberdata <borgeringstad@Brge-Ringstad-PC.local>
2022-11-19 19:52:25 -0500

    created singlepage route + accepting numerical parameters

--- src/components/singlePages/Singlepages.jsx
+++ src/components/singlePages/Singlepages.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import SinglePageSlider from './SinglePageSlider/SinglePageSlider';
 import './singlepage.css'
 const Singlepages = () => {
+    const { id } = useParams(null)
+    const [item, setItem] = useState(null)
+
   return (
-    <div>
-        
-    </div>
+  <> 
+    {item ? (
+        <main>
+            <SinglePageSlider />
+            <div className="container">
+                <section className="mainContent details">
+                    <h1 className="title">
+                        {item.title}
+                    </h1>
+                </section>
+            </div>
+        </main>
+        ) : ( <h1>not found</h1>) }
+  </>
   )
 }
 
